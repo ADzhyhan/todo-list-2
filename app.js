4b5b0240644caa2996bdf963e6a99a7b637ea134
ADzhyhan <a.dzhyhan@gmail.com>
2021-08-18 18:12:32 +0300

    add function for rendering all task in the list

--- app.js
+++ app.js
@@ -31,4 +31,62 @@ const tasks = [
   },
 ];
 
-(function(arrOfTasks) {})(tasks);
+(function(arrOfTasks) {
+  const objOfTasks = arrOfTasks.reduce((acc, task) => {
+    acc[task._id] = task;
+    return acc;
+  }, {});
+
+  renderAllTasks(objOfTasks);
+
+  function renderAllTasks(taskList) {
+    if (!taskList) {
+      console.error('Передайте список задач');
+      return;
+    }
+
+    const fragment = document.createDocumentFragment();
+
+    Object.values(taskList).forEach(task => {
+      const li = listItemTemplate(task);
+      fragment.appendChild(li);
+    });
+
+    function listItemTemplate ({ _id, title, body } = {}) {
+      const li = document.createElement('li'); 
+      li.classList.add(
+        'list-group-item', 
+        'd-flex', 
+        'align-items-center', 
+        'flex-wrap', 
+        'mt-2'
+      );
+
+      const span = document.createElement('span'); 
+      span.textContent = title; 
+      span.style.fontWeight = 'bold'; 
+
+      const deleteBtn = document.createElement('button'); 
+      deleteBtn.textContent = 'Delete task'; 
+      deleteBtn.classList.add(
+        'btn', 
+        'btn-danger', 
+        'ml-auto',
+        'delete-btn'
+      );
+
+      const article = document.createElement('p');
+      article.textContent = body; 
+      article.classList.add(
+        'mt-2', 
+        'w-100'
+      ); 
+
+      li.appendChild(span); 
+      li.appendChild(deleteBtn); 
+      li.appendChild(article); 
+      
+      return li;
+    }
+  }
+})(tasks);
