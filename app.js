9898feb8ef14ef8f8f0c3ca40fe69f29f05574fc
ADzhyhan <a.dzhyhan@gmail.com>
2021-08-19 17:32:57 +0300

    add all functionality for deleting tasks from list

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ const tasks = [
   //Events
   renderAllTasks(objOfTasks);
   form.addEventListener('submit', onFormSubmitHandler);
+  listContainer.addEventListener('click', onDeleteHandler);
 
   function renderAllTasks(taskList) {
     if (!taskList) {
@@ -71,6 +72,7 @@ const tasks = [
       'flex-wrap', 
       'mt-2'
     );
+    li.setAttribute('data-task-id', _id);
 
     const span = document.createElement('span'); 
     span.textContent = title; 
@@ -127,5 +129,32 @@ const tasks = [
     objOfTasks[newTask._id] = newTask; 
 
     return { ...newTask };
+  }
+
+  function deleteTask(id) {
+    const { title } = objOfTasks[id];
+    const isConfirm = confirm(`Do you really want to delete task: "${title}" ?`);
+      
+    if(!isConfirm) {
+      return isConfirm;
+    } else {
+      delete objOfTasks[id];
+      return isConfirm;
+    }
+  } 
+
+  function deleteTaskFromHtml(confirmed, el) {
+    if(!confirmed) return;
+    el.remove();
+  }
+
+  function onDeleteHandler({ target }) {
+    if(target.classList.contains('delete-btn')) {
+      const parent = target.closest('[data-task-id]');
+      const id = parent.dataset.taskId; 
+      const confirmed = deleteTask(id); 
+
+      deleteTaskFromHtml(confirmed, parent);
+    }
   }
 })(tasks);
