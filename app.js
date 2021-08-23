e3720fb8fecf804ff528d422f8e9e650e010268c
ADzhyhan <a.dzhyhan@gmail.com>
2021-08-23 14:47:01 +0300

    update checks if tasklist is empty

--- app.js
+++ app.js
@@ -1,34 +1,4 @@
 const tasks = [
-  {
-    _id: '5d2ca9e2e03d40b326596aa7',
-    completed: true,
-    body:
-      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
-    title: 'Eu ea incididunt sunt consectetur fugiat non.',
-  },
-  {
-    _id: '5d2ca9e29c8a94095c1288e0',
-    completed: false,
-    body:
-      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-    title:
-      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
-  },
-  {
-    _id: '5d2ca9e2e03d40b3232496aa7',
-    completed: true,
-    body:
-      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
-    title: 'Eu ea incididunt sunt consectetur fugiat non.',
-  },
-  {
-    _id: '5d2ca9e29c8a94095564788e0',
-    completed: false,
-    body:
-      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-    title:
-      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
-  },
 ];
 
 (function(arrOfTasks) {
@@ -118,14 +88,17 @@ const tasks = [
   error.style.color = 'red' 
   error.style.fontWeight = 'bold';
 
-  if(!arrOfTasks.length) formWrapper.insertAdjacentElement('afterend', error);
-  themeSelect.value = lastSelectedTheme;
+  
 
   //Events
   setTheme(lastSelectedTheme);
+  themeSelect.value = lastSelectedTheme;
+  
   if(localStorage.getItem('todo')) {
     objOfTasks = JSON.parse(localStorage.getItem('todo'));
     renderAllTasks(objOfTasks);
+  } else {
+    formWrapper.insertAdjacentElement('afterend', error); 
   }
   // renderAllTasks(objOfTasks);
   form.addEventListener('submit', onFormSubmitHandler);
@@ -226,7 +199,6 @@ const tasks = [
     }; 
 
     objOfTasks[newTask._id] = newTask;
-    console.log(objOfTasks);
     localStorage.setItem('todo', JSON.stringify(objOfTasks));
     return { ...newTask };
   }
@@ -240,7 +212,6 @@ const tasks = [
     } else {
       delete objOfTasks[id]; 
       localStorage.setItem('todo', JSON.stringify(objOfTasks));
-      console.log(objOfTasks)
       return isConfirm; 
     }
   } 
